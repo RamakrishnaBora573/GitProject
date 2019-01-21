da5a1accf00f87c4495b61ce90f0bf7d7231f9d6
RAMAKRISHNA BORA <RAMAKRISHNA BORA@192.168.0.5>
2019-01-21 23:40:25 +0530

    commit

---
+++ org/src/main/java/tests/GoogleHomePage.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GoogleHomePage {
+
+	public WebDriver driver;
+	 
+	  @Test
+	  public void openMyBlog() throws Exception {
+		 driver.get("https://www.google.co.in");
+		 driver.manage().window().maximize();		 
+		 Thread.sleep(3000);
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() throws InterruptedException {
+	   
+	   System.setProperty("webdriver.chrome.driver", "C:\\WorkSpace\\chromedriver_win32\\chromedriver.exe");
+	   driver = new ChromeDriver();
+	     
+	  }
+	  
+	  @AfterClass
+	  public void afterClass() {
+	   driver.quit();
+	  }
+}
